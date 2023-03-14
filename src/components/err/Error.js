9cb6f9bf0b0a094623b79d2d6b06f9dc75560e9b
Domains18 <gibsonsgibson88@gmail.com>
2023-03-15 00:21:25 +0300

    err

--- src/components/err/Error.js
+++ src/components/err/Error.js
@@ -5,10 +5,10 @@ const Error = () => {
     return (
         <div className='error-container'>
             <div className="error-title">
-                <h1>Error 404</h1>
+                <h1> <span className='error'>OOPS!!</span> Error 404</h1>
             </div>
             <div className="error-content">
-                <p>Sorry, the page you are looking for does not exist.</p>
+                <p>You found our error page</p>
                 <button className='redirect'>
                     <Link className='redirect' to='/'>Go Back Home</Link>
                 </button>

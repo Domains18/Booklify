84f8c4f84df165222496d9d60db4c52009605e11
Domains18 <gibsonsgibson88@gmail.com>
2023-03-14 23:03:23 +0300

    form

--- src/auth/register.js
+++ src/auth/register.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function Register() {
+
+
+const Register = () => {
     return (
-        <div>Register</div>
+        <div className='Login'>
+            <div className="form">
+                <h1 className="title">
+                    Welcome to Booklify
+                </h1>
+                <form className='login-form'>
+                    <input className='auth' type="text" placeholder='Full Names' />
+                    <input className='auth' type="text" placeholder='Email' />
+                    <input type="password" placeholder='Password' className='auth' />
+                    <input type="password" placeholder=' Confirm Password' className='auth' />
+                    <button type='submit' className='submit btn'>Register</button>
+                </form>
+                <p className='message'>
+                    Have an account? <Link to='/login'>Login</Link>
+                </p>
+            </div>
+        </div>
     )
 }
 
